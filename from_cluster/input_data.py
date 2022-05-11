522e86be9ddba62148d6b25540fd10728f4e5065
netflixfjolnir@gmail.com <netflixfjolnir@gmail.com>
2022-05-11 15:48:27 +0000

    Update input_data.py

--- from_cluster/input_data.py
+++ from_cluster/input_data.py
@@ -29,7 +29,7 @@ try:
   flowrate = float(os.environ['flowrate'])
   runtime = float(os.environ['runtime'])
   minnlobes = float(os.environ['minnlobes'])
-except:
+except: 
   # from variables_xVent_yVent_length_flowrate_runtime_minnlobes_test 339377,380305,0.04,5,660,0.7
   xVent = 339377
   yVent = 380305
